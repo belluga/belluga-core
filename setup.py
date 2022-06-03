43a6484859a98b169122d90f44cc625c8b0ec123
eltonmorais <elton@marketinglucrativo.com>
2022-06-03 09:56:46 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='belluga',
+version='0.1',
+description='Testing installation of Package',
+url='#',
+author='auth',
+author_email='elton@bellugasolutions.com',
+# license='MIT',
+packages=['belluga'],
+zip_safe=False)
