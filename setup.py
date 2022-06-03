6d361ac668be273af7c53116ded613a1f2515c5b
eltonmorais <elton@marketinglucrativo.com>
2022-06-03 14:09:19 -0300

    manifest.in

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
+from pkg_resources import resource_exists
 from setuptools import setup
+
+import belluga
+from belluga import presentation
+from belluga.presentation import connect_api
 setup(name='belluga',
-version='0.3',
+version='0.4',
 description='Testing installation of Package',
 url='#',
 author='auth',
