da26d36c55e038c307bbb1b1fad796febfcb5326
eltonmorais <elton@marketinglucrativo.com>
2022-06-04 12:39:46 -0300

    set connection on parent

--- belluga/presentation/public_api/contracts/module_router.py
+++ belluga/presentation/public_api/contracts/module_router.py
@@ -24,7 +24,6 @@ class ModuleRouter(ABC):
         self.tags = ModuleRouter.tags
         self.prefix = ModuleRouter.prefix
 
-    @abstractmethod
     def set_connection(self, connection: BellugaConnectFactory):
         self.connection = connection
 
