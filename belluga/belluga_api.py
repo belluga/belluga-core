a6eadc8d55ed47116f762cd1d87f7e37a9f33f9b
eltonmorais <elton@marketinglucrativo.com>
2022-06-06 09:22:53 -0300

    fix

--- belluga/belluga_api.py
+++ belluga/belluga_api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from belluga.presentation.connect_api.routes.connection_requests import ConnectionRequestRoute
 
-class BellugaAPI(object):
+class BellugaAPI():
 
     def __init__(self):
         self.api = FastAPI()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pkg_resources import resource_exists
 from setuptools import find_packages, setup
 
 setup(name='belluga',
-version='0.49',
+version='0.50',
 description='Testing installation of Package',
 url='#',
 author='auth',
