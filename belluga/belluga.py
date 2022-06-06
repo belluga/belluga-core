902f39f2b31366097a3c9cd4c34138e4adac5c8f
eltonmorais <elton@marketinglucrativo.com>
2022-06-06 00:11:58 -0300

    fix

--- belluga/belluga.py
+++ belluga/belluga.py
@@ -1,9 +1,5 @@
-from django.conf import settings
 from belluga.application.common.classes.singleton import Singleton
-from belluga.belluga_api import BellugaAPI
 from belluga.infrastructure.dal.contracts.belluga_connect import BellugaConnect
-from belluga.belluga_routes import BellugaRoutes
-from belluga.presentation.connect_api.routes.connection_requests import ConnectionRequestRoute
 
 @Singleton
 class Belluga:
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pkg_resources import resource_exists
 from setuptools import find_packages, setup
 
 setup(name='belluga',
-version='0.43',
+version='0.44',
 description='Testing installation of Package',
 url='#',
 author='auth',
