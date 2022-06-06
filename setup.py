019c9447e0be5903b7c265084ed3fa95187f000a
eltonmorais <elton@marketinglucrativo.com>
2022-06-06 11:18:25 -0300

    clean

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from pkg_resources import resource_exists
 from setuptools import find_packages, setup
 
 setup(name='belluga',
-version='0.57',
+version='0.58',
 description='Testing installation of Package',
 url='#',
 author='auth',
 author_email='elton@bellugasolutions.com',
 packages=find_packages(),
 include_package_data=True,
-# license='MIT',
 zip_safe=False)
