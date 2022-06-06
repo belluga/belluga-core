13b9f72b0c66bc7a5a05cfa7933b0ed9268f5d73
eltonmorais <elton@marketinglucrativo.com>
2022-06-06 11:47:06 -0300

    fix setup

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ packages=find_packages(),
 include_package_data=True,
 zip_safe=False,
 install_requires=[
-    "<fastapi>;python_version<'<0.78.0>'",
-    "<uvicorn>;python_version<'<0.17.6>'",
-    "<python-decouple>;python_version<'<3.6>'",
-    "<fastapi_utils>;python_version<'<0.2.1>'",
-    "<motor>;python_version<'<3.0.0>'"
+    "fastapi>=0.78.0",
+    "uvicorn>=0.17.6",
+    "python-decouple>=3.6",
+    "fastapi_utils>=0.2.1",
+    "motor>=3.0.0"
  ]
 )
