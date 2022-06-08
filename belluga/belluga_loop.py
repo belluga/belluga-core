decf4d02c8de0a02362bd2cfe83c8699d2015446
eltonmorais <elton@marketinglucrativo.com>
2022-06-08 16:42:59 -0300

    start_loop as static

--- belluga/belluga_loop.py
+++ belluga/belluga_loop.py
@@ -4,8 +4,10 @@ import asyncio
 class BellugaLoop(object):
 
     def __init__(self, loop):
-        self.start_loop(loop)
+        pass
 
-    def start_loop(self, loop):
+    
+    @staticmethod
+    def start_loop(loop):
         asyncio.set_event_loop(loop)
         loop.run_forever()
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='belluga',
-version='1.1.12',
+version='1.1.13',
 description='Testing installation of Package',
 url='#',
 author='auth',
