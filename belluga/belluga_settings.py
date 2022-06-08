30454fb09295043b8c4bf2d07075e426aa1b4dae
eltonmorais <elton@marketinglucrativo.com>
2022-06-08 16:13:35 -0300

    fix

--- belluga/belluga_settings.py
+++ belluga/belluga_settings.py
@@ -3,7 +3,7 @@ from belluga.application.common.classes.singleton import Singleton
 class BellugaSettings(Singleton):
 
     def instance(self):
-        return super().instance()
+        return super().instance(self)
 
     def set_db(self, db_settings: dict):
         self.db_settings = db_settings
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='belluga',
-version='1.1.6',
+version='1.1.7',
 description='Testing installation of Package',
 url='#',
 author='auth',
