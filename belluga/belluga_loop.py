d9dfac32ab5a565a11542aea6ccc593a33904a3f
eltonmorais <elton@marketinglucrativo.com>
2022-06-08 16:41:32 -0300

    fix

--- belluga/belluga_loop.py
+++ belluga/belluga_loop.py
@@ -3,22 +3,8 @@ import asyncio
 
 class BellugaLoop(object):
 
-    def __init__(self, cls, loop):
-        self._cls = cls
+    def __init__(self, loop):
         self.start_loop(loop)
-
-    def instance(self, loop):
-        try:
-            return self._instance
-        except AttributeError:
-            self._instance = self._cls(loop)
-            return self._instance
-
-    def __call__(self):
-        raise TypeError('Singletons must be accessed through `instance()`.')
-
-    def __instancecheck__(self, inst):
-        return isinstance(inst, self._cls)
 
     def start_loop(self, loop):
         asyncio.set_event_loop(loop)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='belluga',
-version='1.1.11',
+version='1.1.12',
 description='Testing installation of Package',
 url='#',
 author='auth',
