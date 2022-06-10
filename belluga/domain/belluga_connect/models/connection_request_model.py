65f430f57821c2af6afb3ef0a96ae778bccd3884
eltonmorais <elton@marketinglucrativo.com>
2022-06-10 20:03:35 -0300

    fix

--- belluga/domain/belluga_connect/models/connection_request_model.py
+++ belluga/domain/belluga_connect/models/connection_request_model.py
@@ -18,13 +18,14 @@ class ConnectionRequestModel(BaseModel):
 
     @staticmethod
     def helper(connection_request: dict) -> dict:
+        print(connection_request)
         return ConnectionRequestModel(
             id=str(connection_request["_id"]),
             time=connection_request["time"],
             payload=dict(connection_request["payload"]),
             status=str(connection_request["status"]),
             connection_id=str(connection_request["connection_id"]),
-            attempts=int(connection_request.get["attempts", 0]),
+            attempts=int(connection_request["attempts"]),
             counter=dict(connection_request.get("counter", {
                 "processed": 0,
                 "error": 0,
