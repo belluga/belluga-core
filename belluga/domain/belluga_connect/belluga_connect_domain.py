13e770bf338edc41642ff2a36eced848f9a86f92
eltonmorais <elton@marketinglucrativo.com>
2022-06-10 22:13:04 -0300

    fix

--- belluga/domain/belluga_connect/belluga_connect_domain.py
+++ belluga/domain/belluga_connect/belluga_connect_domain.py
@@ -1,3 +1,5 @@
+from bson import ObjectId
+from belluga.belluga_connection import BellugaConnection
 from belluga.domain.belluga_connect.models.connection_request_model import ConnectionRequestModel
 from belluga.application.common.enums.connection_request_status import ConnectionRequestStatus
 
@@ -8,11 +10,38 @@ class BellugaConnectDomain():
         self.request = request
 
     def counter_status_increment(self, status: ConnectionRequestStatus, increment_value: int = 1):
-        print(self.request.counter)
+        _set = self._counter_status_increment_build_set(
+            status, increment_value)
+        self._save_parent(_set)
 
-        # self.request.counter[status.value] = self.request.counter[status.value] + increment_value
-        self.request.counter_status_increment(status, increment_value)
-        print(self.request.counter)
+    def _counter_status_increment_build_set(self, status: ConnectionRequestStatus, increment_value: int = 1) -> dict:
+        _set = {
+            "$inc": {
+                "counter."+status.value: increment_value
+            }
+        }
 
-    def save(self):
-        self.request.save()
+        return _set
+
+    def _build_match_id(self) -> dict:
+        _match = {
+            '_id': ObjectId(self.id)
+        }
+
+        return _match
+
+    def _build_parent_match_id(self) -> dict:
+        _match = {
+            '_id': ObjectId(self.request.connection_id)
+        }
+
+        return _match
+
+    def _save_parent(self, set: dict):
+        _match = self._build_parent_match_id()
+        _belluga_connection = BellugaConnection()
+        _belluga_connection.connection.update(
+            self.request.parent_collection, _match, set)
+
+    def _save(self):
+        pass
--- belluga/domain/belluga_connect/models/connection_request_model.py
+++ belluga/domain/belluga_connect/models/connection_request_model.py
@@ -38,37 +38,3 @@ class ConnectionRequestModel(BaseModel):
                 ConnectionRequestStatus.received.value: 0
             }))
         )
-
-    def counter_status_increment(self, status: ConnectionRequestStatus, increment_value: int = 1):
-        _set = self._counter_status_increment_build_set(
-            status, increment_value)
-        self._save_parent(_set)
-
-    def _counter_status_increment_build_set(self, status: ConnectionRequestStatus, increment_value: int = 1) -> dict:
-        _set = {
-            "$inc": {
-                "counter."+status.value: increment_value
-            }
-        }
-
-        return _set
-
-    def _build_match_id(self) -> dict:
-        _match = {
-            '_id': ObjectId(self.id)
-        }
-
-        return _match
-
-    def _build_parent_match_id(self) -> dict:
-        _match = {
-            '_id': ObjectId(self.connection_id)
-        }
-
-        return _match
-
-    def _save_parent(self, set: dict):
-        _match = self._build_parent_match_id()
-        _belluga_connection = BellugaConnection()
-        _belluga_connection.connection.update(
-            self.parent_collection, _match, set)
